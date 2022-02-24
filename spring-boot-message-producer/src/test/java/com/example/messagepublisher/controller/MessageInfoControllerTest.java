44fe1052aa3a9cfed67b29b41d67826e9cc1463c
matcha <hzhang1@macalester.edu>
2022-02-24 00:59:51 -0600

    Add producer src code

---
+++ spring-boot-message-producer/src/test/java/com/example/messagepublisher/controller/MessageInfoControllerTest.java
@@ -0,0 +1,17 @@
+package com.example.messagepublisher.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(MockitoJUnitRunner.class)
+public class MessageInfoControllerTest {
+    @Test
+    void test_post_creatMessage_201_SUCCESS() throws Exception {
+
+    }
+}
